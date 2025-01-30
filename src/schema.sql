e29191c613b55dc91841fc04eef4b2e652811458
Hamzah-T <hamzah.tola@ada.ac.uk>
2025-01-30 11:47:48 +0000

    Created and Tested location, members and staff tables

--- src/schema.sql
+++ src/schema.sql
@@ -5,6 +5,7 @@
 .mode column
 
 -- Enable foreign key support
+PRAGMA foreign_keys = ON;
 
 -- Create your tables here
 -- Example:
@@ -13,6 +14,43 @@
 --     column2 datatype,
 --     ...
 -- );
+
+CREATE TABLE locations(
+    location_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'), -- All Numbers should start with 555- and follow with 4 digits
+    email TEXT UNIQUE NOT NULL CHECK(email LIKE '%_@fittrackpro.com'), -- All Location emails should end with @fittrackpro.com 
+    opening_hours TEXT NOT NULL
+);
+
+CREATE TABLE members(
+    member_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL CHECK (email LIKE '%_._@email.com'), -- All member emails should end with ._email.com
+    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'),
+    date_of_birth TEXT NOT NULL,
+    join_date TEXT NOT NULL CHECK(join_date >= date_of_birth),
+    emergency_contact_name TEXT NOT NULL,
+    emergency_contact_phone TEXT NOT NULL CHECK(emergency_contact_phone LIKE '555-____')
+);
+
+CREATE TABLE staff(
+    staff_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL CHECK (email LIKE '%_._@fittrackpro.com'),--All staff emails should end with ._fittrackpro.com
+    phone_number TEXT NOT NULL CHECK(phone_number LIKE '555-____'),
+    position TEXT NOT NULL CHECK(position IN ('Trainer', 'Manager', 'Receptionist', 'Maintenance')),
+    hire_date TEXT NOT NULL,
+    location_id INTEGER,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id) ON UPDATE CASCADE --Ensures location_id changes update all dependent records accordingly
+    ON DELETE SET NULL -- Prevents orphaned records by setting location_id to NULL when location is deleted
+);
+
+
+
 
 -- TODO: Create the following tables:
 -- 1. locations
